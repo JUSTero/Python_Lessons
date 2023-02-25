6736cc94bcd9f98a6072df17a6da05a2947f9e1a
JUSTero <skorneta@mail.ru>
2023-02-25 14:54:56 +0300

    Task 2 complete again

--- PythonSeminar_1/Seminar_3/Seminar_3.py
+++ PythonSeminar_1/Seminar_3/Seminar_3.py
@@ -2,9 +2,11 @@
 
 # Задача 1. Дан список чисел. Определите, сколько в нем встречается различных чисел.
 
-# Задача 2.  Дана последовательность из N целых чисел и число K. 
+# Задача 2. Дана последовательность из N целых чисел и число K. 
 # Необходимо сдвинуть всю последовательность (сдвиг - циклический)
 # на K элементов вправо, K – положительное число.
+
+# Задача 3. Напишите программу для печати всех уникальных значений в словаре.
 
 # Task 1
 #import random
@@ -19,13 +21,17 @@ import random
 
 some_list = [random.randint(1, 10)for i in range(int(input("Введите длину списка: ")))]
 K = int(input('Введите размер сдвига: '))
-temp = K
 print(f'Начальный список: {some_list}')
 
-for i in range(K):
-	some_list.insert(i,some_list[len(some_list) - K])
-	K -= 1
+#for i in range(K):
+#	some_list.insert(i,some_list[len(some_list) - K])
+#	del some_list[len(some_list) - K]
+#	K -= 1
 
-del some_list[-temp:len(some_list)]
+#print(f'Список со сдвигом: {some_list}')
 
-print(f'Список со сдвигом: {some_list}')
+final_list = some_list[K:] + some_list[:K]
+print(f'Список со сдвигом: {final_list}')
+
+# Task 3
+
