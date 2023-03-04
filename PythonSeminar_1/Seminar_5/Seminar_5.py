95413c08f74780ad79de2761984aa0469098d00f
JUSTero <skorneta@mail.ru>
2023-03-04 12:05:14 +0300

    Task 1 complete

---
+++ PythonSeminar_1/Seminar_5/Seminar_5.py
@@ -0,0 +1,35 @@
+# -*- coding: cp1251 -*-
+
+# Задача 1.  Последовательностью Фибоначчи называется последовательность чисел a0, a1, ..., an,
+#  ..., где a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1). Требуется найти N-е число Фибоначчи
+
+#N = int(input(f'Введите число: '))
+
+#def fib(n):
+#	a = 0
+#	b = 1
+#	f = 0
+
+#	if n == 1: 
+#		print(b)
+#	elif n ==0: 
+#		print(a)
+#	else:
+#		for i in range(2, n + 1):
+#			f = a + b
+#			a = b
+#			b = f
+#	return f
+
+#print(fib(N))
+
+#N = int(input(f'Введите число: '))
+
+#def fib(n):
+#	if n == 0: 
+#		return 0
+#	elif n == 1: 
+#		return 1
+#	return fib(n - 1) + fib(n - 2)
+
+#print(fib(N))
